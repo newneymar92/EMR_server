4d2762e36b3082c4bed94a920d39573ce67fe215
newneymar92 <nguyenanhtai.hust@gmail.com>
2023-02-05 00:41:25 +0700

    vercel

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json({ limit: "30mb" }));
 app.use(express.urlencoded({ extended: true, limit: "30mb" }));
 
-// app.use("/emr_ibme", posts);
+app.use("/emr_ibme", posts);
 
 app.get("/", (req, res) => {
   res.send("Success");
